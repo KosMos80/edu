0f98656d0f0d151ea096d71f6682bbfdbaf3049d
KosMos80 <107849287+KosMos80@users.noreply.github.com>
2022-07-14 17:03:04 +0300

    input coordinat

--- main.cpp
+++ main.cpp
@@ -2,9 +2,27 @@
 #include <stdio.h>
 
 
+struct Target
+{
+    int posx;
+    int posy;
+    int posz;
+};
+
+Target point1, point2;
 
 int main(void)
 {
+    std::cout << "Enter coor point1 "<<std::endl;
+    std::cout << "x:"; std::cin >> poin1.posx;
+    std::cout << "y:"; std::cin >> poin1.posy;
+    std::cout << "z:"; std::cin >> poin1.posz;
+    std::cout << "Enter coor point2 "<<std::endl;
+    std::cout << "x:"; std::cin >> poin2.posx;
+    std::cout << "y:"; std::cin >> poin2.posy;
+    std::cout << "z:"; std::cin >> poin2.posz;
+    std::cout << "=============================" << std::endl;
+    
 
 return 0
 }
